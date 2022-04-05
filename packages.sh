4c4ff34c265d3555700d3a29fdc2d0eecfc109f8
谢箫生 <64482026+xiexiaosheng@users.noreply.github.com>
2022-04-05 16:47:43 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-vlmcsd ./luci-app-vlmcsd
-git clone -b dev https://github.com/vernesong/OpenClash
+#openclash
+rm -rf luci-app-openclash
+rm -rf OpenClash
+git clone https://github.com/vernesong/OpenClash.git
 mv -f OpenClash/luci-app-openclash .
 rm -rf OpenClash
-git clone https://github.com/xiexiaosheng/helloworld
-
+#KMS
+svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-vlmcsd
+svn co https://github.com/coolsnowwolf/packages/trunk/net/vlmcsd
+#SSR-PLUS
+svn co https://github.com/xiexiaosheng/helloworld/trunk ./
+#½áÊø
 rm -rf .svn
 rm -rf ./*/.git
 rm -f .gitattributes .gitignore
