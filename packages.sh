91fd6255af63b407ed9fd00c594cee87ee912866
谢箫生 <64482026+xiexiaosheng@users.noreply.github.com>
2022-04-05 20:03:39 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -8,9 +8,11 @@ rm -rf OpenClash
 #KMS
 svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-vlmcsd
 svn co https://github.com/coolsnowwolf/packages/trunk/net/vlmcsd
+rm -rf luci-app-vlmcsd/.svn/wc.db
+rm -rf vlmcsd/.svn/wc.db
 #SSR-PLUS
 svn co https://github.com/xiexiaosheng/helloworld/trunk ./
-#½áÊø
+#exit
 rm -rf .svn
 rm -rf ./*/.git
 rm -f .gitattributes .gitignore
