a91db53d451a9844b53f02933cb9b2ebf65e7f9f
谢箫生 <64482026+xiexiaosheng@users.noreply.github.com>
2022-04-08 13:45:26 +0800

    2022年4月8日

--- packages.sh
+++ packages.sh
@@ -6,10 +6,10 @@ git clone -b dev https://github.com/vernesong/OpenClash.git
 mv -f OpenClash/luci-app-openclash .
 rm -rf OpenClash
 #KMS
-svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-vlmcsd
-svn co https://github.com/coolsnowwolf/packages/trunk/net/vlmcsd
+svn co https://github.com/ysc3839/trunk/openwrt-vlmcsd
+svn co https://github.com/cokebar/trunk/luci-app-vlmcsd
+rm -rf openwrt-vlmcsd/.svn/wc.db
 rm -rf luci-app-vlmcsd/.svn/wc.db
-rm -rf vlmcsd/.svn/wc.db
 #SSR-PLUS
 svn co https://github.com/xiexiaosheng/helloworld/trunk ./
 #exit
