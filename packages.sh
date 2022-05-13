6530843db7c50085402de73a2f583524b21a27cf
谢箫生 <64482026+xiexiaosheng@users.noreply.github.com>
2022-05-13 14:48:49 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -5,9 +5,6 @@ rm -rf OpenClash
 git clone -b dev https://github.com/vernesong/OpenClash.git
 mv -f OpenClash/luci-app-openclash .
 rm -rf OpenClash
-#KMS
-git clone https://github.com/ysc3839/openwrt-vlmcsd
-git clone https://github.com/cokebar/luci-app-vlmcsd
 #SSR-PLUS
 svn co https://github.com/fw876/helloworld/trunk ./
 svn co https://github.com/coolsnowwolf/packages/trunk/net/dns2socks
@@ -15,7 +12,6 @@ svn co https://github.com/coolsnowwolf/packages/trunk/net/ipt2socks
 svn co https://github.com/coolsnowwolf/packages/trunk/net/microsocks
 svn co https://github.com/coolsnowwolf/packages/trunk/net/pdnsd-alt
 svn co https://github.com/coolsnowwolf/packages/trunk/net/redsocks2
-#svn co https://github.com/xiexiaosheng/helloworld/trunk ./
 #luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon
 #exit
