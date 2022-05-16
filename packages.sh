b8529428a083a87d51477a9011a9878737491eaf
谢箫生 <64482026+xiexiaosheng@users.noreply.github.com>
2022-05-16 14:16:14 +0800

    更换源
    2022年5月16日

--- packages.sh
+++ packages.sh
@@ -7,13 +7,6 @@ mv -f OpenClash/luci-app-openclash .
 rm -rf OpenClash
 #SSR-PLUS
 svn co https://github.com/fw876/helloworld/trunk ./
-svn co https://github.com/coolsnowwolf/packages/trunk/net/dns2socks
-svn co https://github.com/coolsnowwolf/packages/trunk/net/ipt2socks
-svn co https://github.com/coolsnowwolf/packages/trunk/net/microsocks
-svn co https://github.com/coolsnowwolf/packages/trunk/net/pdnsd-alt
-svn co https://github.com/coolsnowwolf/packages/trunk/net/redsocks2
-#luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon
 #exit
 rm -rf .svn
 rm -rf ./*/.svn
