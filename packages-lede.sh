c296ab001ff1d99ea16bdf4b6b32795f29516a2d
谢箫生 <64482026+xiexiaosheng@users.noreply.github.com>
2022-07-17 11:43:50 +0800

    切换源

--- packages-lede.sh
+++ packages-lede.sh
@@ -7,11 +7,6 @@ mv -f OpenClash/luci-app-openclash .
 rm -rf OpenClash
 #SSR-PLUS
 svn co https://github.com/fw876/helloworld/trunk ./
-svn co https://github.com/coolsnowwolf/packages/trunk/net/dns2socks
-svn co https://github.com/coolsnowwolf/packages/trunk/net/ipt2socks
-svn co https://github.com/coolsnowwolf/packages/trunk/net/microsocks
-svn co https://github.com/coolsnowwolf/packages/trunk/net/pdnsd-alt
-svn co https://github.com/coolsnowwolf/packages/trunk/net/redsocks2
 #exit
 rm -rf .svn
 rm -rf ./*/.svn
