1da1b823fb7e5860e4358487ee696b8f58397e44
谢箫生 <64482026+xiexiaosheng@users.noreply.github.com>
2022-07-17 14:02:55 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -12,6 +12,11 @@ svn co https://github.com/coolsnowwolf/packages/trunk/net/ipt2socks
 svn co https://github.com/coolsnowwolf/packages/trunk/net/microsocks
 #svn co https://github.com/coolsnowwolf/packages/trunk/net/pdnsd-alt
 svn co https://github.com/coolsnowwolf/packages/trunk/net/redsocks2
+#luci-app-diskman
+rm -rf luci-app-diskman
+git clone https://github.com/lisaac/luci-app-diskman diskman
+mv -f diskman/applications/luci-app-diskman .
+rm -rf diskman
 #exit
 rm -rf .svn
 rm -rf ./*/.svn
